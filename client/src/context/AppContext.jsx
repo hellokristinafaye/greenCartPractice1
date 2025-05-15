596f36b11f2ce1445942c63e3bed96481ac13919
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-15 14:12:08 -0700

    state variables added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,10 +1,16 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
     
-    const value = {}
+    const value = {navigate, user, setUser, isSeller, setIsSeller}
+
     return <AppContextProvider value={value}>
         {children}
     </AppContextProvider>
